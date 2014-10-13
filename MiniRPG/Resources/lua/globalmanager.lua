17128ded4badb329c25a7221c6aa6228411e9a57
bluemaxmb <mbarney@Inhance-Mac-Mini-900.local>
2014-10-13 12:07:38 -0700

    Redid the project to actually use Lua properly

--- MiniRPG/Resources/lua/globalmanager.lua
+++
@@ -1,6 +0,0 @@
---Global Lua Manager
-gNPCLUT = {}
-gChestLUT = {}
-gQuestFlagsLUT = {["TalkedToKingForFirstTime"] = false,}
-
---print("result is " .. 2)
--- MiniRPG/cocos2d/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.cpp
+++
@@ -1,6 +0,0 @@
-#include "lua_cocos2dx_custom.hpp"
-#include "NPCManager.h"
-#include "tolua_fix.h"
-#include "LuaBasicConversions.h"
-
-
