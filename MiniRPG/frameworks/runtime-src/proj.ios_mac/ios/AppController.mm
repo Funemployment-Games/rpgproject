7a4e40f7e124cd7b7d3df04d9b9ab1fc332607d8
M B <bluemaxmb@gmail.com>
2014-10-23 10:33:10 -0700

    # Updated to Cocos2d-x 3.3 rc0
    That tile map staggering is probably going to be useful… Plus the way
    they redid the Lua stuff should be cleaner. And the new audio engine.

--- MiniRPG/frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
+++ MiniRPG/frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
@@ -1,5 +1,6 @@
 /****************************************************************************
- Copyright (c) 2010 cocos2d-x.org
+ Copyright (c) 2010-2013 cocos2d-x.org
+ Copyright (c) 2013-2014 Chukong Technologies Inc.
 
  http://www.cocos2d-x.org
 
@@ -22,11 +23,13 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#import "AppController.h"
-#import "CCEAGLView.h"
+#import <UIKit/UIKit.h>
 #import "cocos2d.h"
+
+#import "AppController.h"
 #import "AppDelegate.h"
 #import "RootViewController.h"
+#import "CCEAGLView.h"
 
 @implementation AppController
 
@@ -36,49 +39,49 @@
 // cocos2d application instance
 static AppDelegate s_sharedApplication;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
 
     // Override point for customization after application launch.
 
     // Add the view controller's view to the window and display.
     window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
-
-    // Init the CCEAGLView
     CCEAGLView *eaglView = [CCEAGLView viewWithFrame: [window bounds]
                                      pixelFormat: kEAGLColorFormatRGBA8
-                                     depthFormat: 0//GL_DEPTH24_STENCIL8_OES
+                                     depthFormat: GL_DEPTH24_STENCIL8_OES
                               preserveBackbuffer: NO
                                       sharegroup: nil
                                    multiSampling: NO
-                                 numberOfSamples: 0];
+                                 numberOfSamples: 0 ];
 
-    // Use RootViewController manage CCEAGLView 
-    _viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
-    _viewController.wantsFullScreenLayout = YES;
-    _viewController.view = eaglView;
+    [eaglView setMultipleTouchEnabled:YES];
+    
+    // Use RootViewController manage CCEAGLView
+    viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
+    viewController.wantsFullScreenLayout = YES;
+    viewController.view = eaglView;
 
     // Set RootViewController to window
     if ( [[UIDevice currentDevice].systemVersion floatValue] < 6.0)
     {
         // warning: addSubView doesn't work on iOS6
-        [window addSubview: _viewController.view];
+        [window addSubview: viewController.view];
     }
     else
     {
         // use this method on ios6
-        [window setRootViewController:_viewController];
+        [window setRootViewController:viewController];
     }
-
+    
     [window makeKeyAndVisible];
 
-    [[UIApplication sharedApplication] setStatusBarHidden:true];
+    [[UIApplication sharedApplication] setStatusBarHidden: YES];
 
     // IMPORTANT: Setting the GLView should be done after creating the RootViewController
     cocos2d::GLView *glview = cocos2d::GLView::createWithEAGLView(eaglView);
     cocos2d::Director::getInstance()->setOpenGLView(glview);
 
     cocos2d::Application::getInstance()->run();
-
     return YES;
 }
 
@@ -88,21 +91,19 @@ static AppDelegate s_sharedApplication;
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-     //We don't need to call this method any more. It will interupt user defined game pause&resume logic
-    /* cocos2d::Director::getInstance()->pause(); */
+    cocos2d::Director::getInstance()->pause();
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-     //We don't need to call this method any more. It will interupt user defined game pause&resume logic
-    /* cocos2d::Director::getInstance()->resume(); */
+    cocos2d::Director::getInstance()->resume();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
      */
     cocos2d::Application::getInstance()->applicationDidEnterBackground();
@@ -130,13 +131,14 @@ static AppDelegate s_sharedApplication;
     /*
      Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
      */
+     cocos2d::Director::getInstance()->purgeCachedData();
 }
 
 
 - (void)dealloc {
-    [window release];
     [super dealloc];
 }
 
 
 @end
+
--- MiniRPG/frameworks/runtime-src/proj.ios_mac/ios/main.m
+++ MiniRPG/frameworks/runtime-src/proj.ios_mac/ios/main.m
@@ -1,3 +1,10 @@
+//
+//  main.m
+//  MiniRPG
+//
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
